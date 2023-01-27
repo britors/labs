7bb201e464d982a9d7570889c2a19551502a1c6b
Rodrigo Brito <rodrigo@w3ti.com.br>
2023-01-27 10:27:47 -0300

    feat: Adicionar configuração para servidor postgresql

---
+++ Databases/Postgres/Postgres01/provision/create_vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Atualizando o sistema
+sudo apt update
+sudo apt full-upgrade -y
+
+# Instalando o PostgreSQL
+sudo apt install postgresql postgresql-contrib -y
+
+# Configurando o PostgreSQL
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Postgres@123';"
+sudo cp /opt/database/config/pg_hba.conf /etc/postgresql/12/main/pg_hba.conf
+sudo cp /opt/database/config/postgresql.conf /etc/postgresql/12/main/postgresql.conf
+
+# Ativando o PostgreSQL
+sudo systemctl restart postgresql
