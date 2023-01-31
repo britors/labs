f57e681821262f489352e6213c2c0431fd29dce0
Rodrigo Brito <rodrigo@w3ti.com.br>
2023-01-31 13:44:12 -0300

    Refactor: Melhorar a criação da maquina postgres

--- Databases/Postgres/Postgres01/provision/create_vm.sh
+++ Databases/Postgres/Postgres01/provision/create_vm.sh
@@ -12,8 +12,8 @@ sudo apt install postgresql postgresql-contrib -y
 
 # Configurando o PostgreSQL
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Postgres@123';"
-sudo cp /opt/database/config/pg_hba.conf /etc/postgresql/12/main/pg_hba.conf
-sudo cp /opt/database/config/postgresql.conf /etc/postgresql/12/main/postgresql.conf
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/12/main/postgresql.conf
+sudo sed -i 's/127.0.0.1\'/'32/0.0.0.0\'/'0/g' /etc/postgresql/12/main/pg_hba.conf
 
 # Ativando o PostgreSQL
 sudo systemctl restart postgresql
